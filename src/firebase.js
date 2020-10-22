d56b3cee7553953f6d96c30a64ebd4fba023e370
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-22 09:36:43 +0530

    Update firebase.js
    You should not give your firebase config in open.

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDEdWdX8R_5HItbMgo--Ez0A1fOBXrG-Mk',
-  authDomain: 'telegram-clone-97494.firebaseapp.com',
-  databaseURL: 'https://telegram-clone-97494.firebaseio.com',
-  projectId: 'telegram-clone-97494',
-  storageBucket: 'telegram-clone-97494.appspot.com',
-  messagingSenderId: '514987536230',
-  appId: '1:514987536230:web:4cdf7239105bb0ff3de2a7',
-  measurementId: 'G-C4TR2J3LB9',
+  // ENTER YOUR APP'S FIREBASE CONFIG HERE
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
